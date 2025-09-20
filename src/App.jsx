2ea5e841f9f9a5901d3517c4828cecd11c4c40d1
QodirovDeveloper <startfly58@gmail.com>
2025-09-20 19:16:49 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import
+{
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { Home, Profile, SingleImage, Login, Signup } from "./pages/index";
+import { useDispatch, useSelector } from "react-redux";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import MainLayout from "./layout/MainLayout";
+import { login, authReady } from "./app/features/userSlice";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/config";
+import { useState } from "react"; // ⬅️ qo‘shing
+
+import Collections from "./pages/Collections";
+import DownloadHistory from "./pages/DownloadHistory";
+import Illustrations from "./pages/Illustrations";
+import GetPlan from "./pages/GetPlan";
+import AddImage from "./pages/AddImage";
+
+
+function App()
+{
+
+  const [searchQuery, setSearchQuery] = useState(""); // ⬅️ holat qo‘shish
+
+  const handleSearch = (query) =>
+  {
+    setSearchQuery(query);
+  };
+
+
+  const dispatch = useDispatch();
+  const { user, isAuthReady } = useSelector((store) => store.user);
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoutes user={user}>
+          <MainLayout searchQuery={searchQuery} onSearch={handleSearch} />
+        </ProtectedRoutes>
+      ),
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/profile",
+          element: <Profile />,
+        },
+        {
+          path: "/singleImage/:id",
+          element: <SingleImage />,
+        },
+        { path: "/collections", element: <Collections /> },
+        { path: "/downloadHistory", element: <DownloadHistory /> },
+        { path: "/illustrations", element: <Illustrations /> },
+        { path: "/getPlan", element: <GetPlan /> },
+        { path: "/addImage", element: <AddImage /> },
+      ],
+    },
+    {
+      path: "/login",
+      element: user ? <Navigate to="/" /> : <Login />,
+    },
+    {
+      path: "/signup",
+      element: user ? <Navigate to="/" /> : <Signup />,
+    },
+  ]);
+
+  onAuthStateChanged(auth, (user) =>
+  {
+    if (user?.displayName && user?.photoURL)
+    {
+      dispatch(login(user));
+    }
+    dispatch(authReady());
+  });
+
+  return <>{isAuthReady && <RouterProvider router={routes} />}</>;
+}
+
+export default App;
+
+// const ACCESS_KEY = "O2XRyjimvUwQaxKL4TysGBBPrQEa-1mb4d-FKIx3-pY";
+// `https://api.unsplash.com/search/photos?query=${seatchParams}&client_id=` +
